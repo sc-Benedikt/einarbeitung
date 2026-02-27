376cbb3954eb8f53819c40b2d976f24edd08b865
root <root@SCT-IIS-DEV-BM01.scaltel.de>
2026-02-27 11:49:42 +0100

    API funktioniert über website

--- 07_API/app.py
+++ 07_API/app.py
@@ -1,28 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import sqlite3
 
 app = Flask(__name__)
 
-if __name__  == "__main__":
-
-    app.run(debug=True)
 
 def init_db():
     connection = sqlite3.connect("base.db")
     cursor = connection.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS items(
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS items(
                         id INTEGER PRIMARY KEY,
                         type TEXT,
-                        price INTEGER)""")
+                        price INTEGER)"""
+    )
 
     cursor.execute("SELECT COUNT(*) FROM items")
     count = cursor.fetchone()[0]
     if count == 0:
-        cursor.execute("INSERT INTO items (type, price) VALUES (?, ?)", ("Laptop", 1200))
-        cursor.execute("INSERT INTO items (type, price) VALUES (?, ?)", ("maus", 20))
-        cursor.execute("INSERT INTO items (type, price) VALUES (?, ?)", ("screen", 300))
+        values = [("Laptop", 1849.99), ("maus", 19.99), ("screen", 249.99)]
+        cursor.executemany(
+            "INSERT INTO items (type, price) VALUES (?, ?) ", values,
+        )
+
     connection.commit()
     connection.close()
+
 
 init_db()
 
@@ -33,15 +35,25 @@ def get_all_items():
     cursor = connection.cursor()
 
     rows = cursor.execute("SELECT * FROM items").fetchall()
-    items  = [{"id" : r[0], "type":r[1],"price":r[2]} for r in rows]
     connection.close()
-    return jsonify(items) 
+    items = """"""
+    for r in rows:
+        items += f"\n id: {r[0]}, type: {r[1]}, price: {r[2]},    "
+    print(items)
+    return items
+
 
 @app.route("/items/<int:item_id>")
 def get_item(item_id):
-    connection = sqlite3.connect("base.db")
-    cursor = connection.cursor()
-    return cursor.execute("SELECT * FROM items WHERE id = (?)", (item_id,)).fetchall()
-  
-    return ("item nicht gefunden")
+    try:
+        connection = sqlite3.connect("base.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM items WHERE id = (?)", (item_id,))
+        option_choice = cursor.fetchall()[0]
+        return f"id: {option_choice[0]}, type: {option_choice[1]}, price: {option_choice[2]} "
+    except IndexError:
+        return "Item not found"
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
